c83a9209dec3485c6a0ad0fe1d0ca49b92da0049
GreggtheDev <greggthedev@gmail.com>
2024-07-15 13:16:08 -0400

    notated index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,19 @@
 const { Sequelize } = require("sequelize");
+// Imports the Sequelize library, which is used to interact with the database.
+
 const sequelize = new Sequelize({
   dialect: "sqlite",
+  // Specifies the type of database to use, in this case, SQLite.
   storage: "./database.sqlite",
+  // Specifies the file path where the SQLite database will be stored.
 });
 
 const FavoritePair = require("./favoritePair")(sequelize);
+// Imports the FavoritePair model, passing the Sequelize instance to the function that defines the model.
 
 module.exports = {
   sequelize,
+  // Exports the Sequelize instance for use in other parts of the application.
   FavoritePair,
+  // Exports the FavoritePair model for use in other parts of the application.
 };
